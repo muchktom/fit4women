ab22dddb89fe4c3122a5ea536f6f58a8551b2cf3
muchktom <tomas.muchka@gooddata.com>
2026-07-25 11:52:41 +0200

    Refactor code structure for improved readability and maintainability

--- web/src/_data/site.js
+++ web/src/_data/site.js
@@ -29,6 +29,7 @@ module.exports = {
     phoneHref: "tel:+420724179352",
     instagram: "https://www.instagram.com/fit4women_ujezd_nad_lesy/",
     facebook: "https://www.facebook.com/groups/1038345606504632/",
+    googleMaps: "https://www.google.com/maps/place/FIT4WOMEN/@49.490399,11.8290176,8z/data=!4m12!1m2!2m1!1sfit4women!3m8!1s0x470b8d5b71f2999b:0xe035f19b61512a3a!8m2!3d50.0778829!4d14.653548!9m1!1b1!15sCglmaXQ0d29tZW5aCyIJZml0NHdvbWVukgEXd29tZW5zX3BlcnNvbmFsX3RyYWluZXKaAURDaTlEUVVsUlFVTnZaRU5vZEhsalJqbHZUMjFTV0ZkRmNGZE9WMlJXVkd0Vk0xVldSbE5UYlhRMFlUSTFUR0ZIWXhBQuABAPoBBAgAEDk!16s%2Fg%2F11z7d12vmw",
     whatsapp: "https://chat.whatsapp.com/HD25aLuCJSO6bPWJ5kEDDp", // ⚠️ ověřit platnost
     locations: [
       { name: "Újezd nad Lesy", detail: "hřiště ul. Polesná" },
